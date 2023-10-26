ed3d97a283a2d19c123e32b6633314838223c995
kimdaeyeub <kdy3915@naver.com>
2023-10-26 22:57:45 +0900

    SetUp

---
+++ src/Router.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+
+function Router() {
+  <BrowserRouter>
+    <Switch>
+      <Route path="/:coinId">
+        <Coin />
+      </Route>
+      <Route path="/">
+        <Coins />
+      </Route>
+    </Switch>
+  </BrowserRouter>;
+}
+
+export default Router;
